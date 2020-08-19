9349990942c8afdb6fdd366ce3695e77f378c2ef
Dioguardi, Fabio <fabiod@bgs.ac.uk>
2020-08-19 10:41:16 +0100

    Updated headers and license document

--- FoxScreen.py
+++ FoxScreen.py
@@ -4,12 +4,11 @@
 -program which displays the output of REFIR on the screen -
  
 Copyright (C) 2020 Tobias Dürig, Fabio Dioguardi
-================================
-
+==============                     ===================
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+at your option any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,15 +16,17 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 If you wish to contribute to the development of REFIR or to reports bugs or other problems with
 the software, please write an email to me.
 
 Contact: tobi@hi.is, fabiod@bgs.ac.uk
 
-RLZ170318M
+
+RNZ170318FS
 """
+
 import datetime
 
 # try:
